3fab873852aadcf026c0bc0bbe83540cf327e1b5
alerenz <99068720+alerenz@users.noreply.github.com>
2023-02-18 12:52:40 +0400

    Delete mini-oop directory

--- mini-oop/groups.py
+++
@@ -1,8 +0,0 @@
-class Groups:
-    amount = 0
-
-    def __init__(self, name_group):
-        self.name_group = name_group
-
-    def print_inf(self):
-        print(f"Группа: {self.name_group}, количевство студентов: {self.amount}")
--- mini-oop/students.py
+++
@@ -1,8 +0,0 @@
-class Students():
-    count = 0
-    def __init__(self, surname,name,group):
-        self.name = name
-        self.surname = surname
-        self.group = group
-    def print_inf(self):
-        print(f"{self.surname} {self.name} {self.group}")
